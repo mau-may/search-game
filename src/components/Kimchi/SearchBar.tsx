45c267b0c06c394e571b44fc78496d157eac9afb
mau <mauuovo@gmail.com>
2023-12-14 18:00:20 +0900

    design: 김치게임 search 퍼블리싱 진행중

--- src/components/Kimchi/SearchBar.tsx
+++ src/components/Kimchi/SearchBar.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { GameSearch } from '../../apis/GameSearch';
+import searchThinking from '../../assets/images/kimchi/thinking.png';
+import searchTextBox from '../../assets/images/kimchi/search_textbox.png';
+import searchImg from '../../assets/images/kimchi/search_img.png';
 import SearchResult from './SearchResult';
 import SearchKeyword from './SearchKeyword';
+import { useTranslation } from "react-i18next";
 
 const SearchBar = ({searchList, setSearchList, inputList, setInputList }:{searchList:any, setSearchList:any, inputList:any, setInputList:any}) => {
+	const { t } = useTranslation();
+
 	const KIMCHI_KEYWROD:string = "김치";
 	const [orgKw, setKw]:any = useState('');
 	const navigate = useNavigate();
@@ -99,13 +105,16 @@ const SearchBar = ({searchList, setSearchList, inputList, setInputList }:{search
 
 	
 	return (
-		<div>
+		<div className="search-bar-wrap">
 			<form>
 				<div className="search-bar">
-					<input type="text" value={orgKw} onChange={Keyword} />
+					<div className="text-bg"><img src={searchTextBox} /></div>
+					<input type="text" value={orgKw} onChange={Keyword} placeholder={t(`kimchi.input-placeholder`)} maxLength={10} />
+					<div className="text-thinking"><img src={searchThinking} /></div>
 				</div>
+				<div className="search-img"><img src={searchImg} /></div>
 				
-				<button type="submit" className="btn primary" onClick={fnGetList}>검색</button>
+				<button type="submit" className="btn primary" onClick={fnGetList}>{t(`common.search`)}</button>
 				{/* <SearchResult 	data={searchList} keyword={orgKw}  /> */}
 			</form>
 			
